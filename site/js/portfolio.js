3c5441e52026f52d4d3cb1982f90d7e8da722f50
ViriatoF <viriato.ferreira44@gmail.com>
2024-10-21 13:23:07 +0200

    finish JS step 2 in part 1

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,8 +1,24 @@
 const imgAvatar = document.querySelector(".avatar");
 
+let nameUser = document.querySelector("#firstname");
+
+const buttonModifyer = document.querySelector(".button_modifyer");
+
+let newName = ""
+
 function changeAvatar() {
   imgAvatar.src = "/site/image/avatar-bis.png"
   console.log("coucou")
 }
 
-imgAvatar.addEventListener("click", changeAvatar)
+imgAvatar.addEventListener("click", changeAvatar);
+
+function changeNameColor() {
+
+  newName = prompt("Enter your name");
+  nameUser.style.color = "white";
+  nameUser.innerHTML = newName;
+  
+};
+
+buttonModifyer.addEventListener("click", changeNameColor);
