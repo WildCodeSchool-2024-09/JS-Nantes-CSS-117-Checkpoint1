6e4831db305724b1e99d0425b7da5b6d5662b6e5
CyrusWeir <cyrusweir@gmail.com>
2024-10-21 12:36:00 +0200

    almost finished the basic JS exercises. I'm just struggling to understand how the variables are attributed, and how they behave when changed with JS

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,5 +1,42 @@
-const avatarElement = document.getElementById("avatar")
+const avatarElement = document.getElementById("avatar");
 
-avatarElement.addEventListener("click") function() {
-  
-}
+// step 1
+
+const avatar1 = "/site/image/avatar-bis.png";
+const avatar2 = "/site/image/avatar.svg";
+
+avatarElement.addEventListener("click", function () {
+  if (avatarElement.src === avatar1) {
+    console.log("change 2")
+    avatarElement.src = avatar2
+  }
+  else if (avatarElement.src === avatar2) {
+    console.log("change 1")
+    avatarElement.src = avatar1
+  }
+  else {
+    console.log("else statement")
+    avatarElement.src = avatar1
+  }
+
+})
+
+// button
+
+const changeButton = document.getElementById("change-name");
+const userDisplay = document.getElementById("firstname")
+const textBackground = document.querySelector(".pink-bg")
+const pinkText = document.querySelector(".pink-text")
+
+changeButton.addEventListener("click", function () {
+  userName = prompt("what's your name?")
+  userDisplay.innerHTML = userName
+  userDisplay.style.color = "white"
+  const newColor = prompt("Enter a color")
+  textBackground.style.cssText = `--lightWildColor: ${newColor}`
+  pinkText.style.cssText = `--lightWildColor: ${newColor}`  
+
+})
+
+
+
