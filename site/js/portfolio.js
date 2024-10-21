11a932f2f41c0a9d4c611af30fab9a68253cfae3
Clément Praud <clement.praud6@gmail.com>
2024-10-21 13:04:33 +0200

    Finishe bonus step 6 on JS website

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -10,11 +10,23 @@ const backgroundBanner=document.getElementById('background-banner');
 const textAreas = document.getElementsByTagName("h4");
 const niceButtons = document.querySelectorAll('.nice-buttons');
 const backgroundBottom=document.getElementById('bottom-background')
+const links = document.querySelectorAll(".link")
+const listToModify = document.getElementById("front-dev-tools")
+const buttonModify = document.getElementById("selection-button");
+const list1 = document.getElementById("li-1");
+const list2 = document.getElementById("li-2");
+const list3 = document.getElementById("li-3");
 
-console.log(textAreas.length);
-console.log(textAreas)
-console.log(niceButtons)
-console.log(backgroundBanner)
+
+buttonModify.addEventListener("click", () => {
+  list1.textContent="";
+  list1.textContent="VSCode"
+  list2.textContent="";
+  list2.textContent="GitHub"
+  list3.textContent="";
+  list3.textContent="Terminal"
+})
+
 
 buttonChangeProfil.addEventListener('click', () => {
   const backgroundColor=prompt('Veuillez choisir la couleur du fond')
@@ -33,6 +45,10 @@ buttonChangeProfil.addEventListener('click', () => {
     niceButtons[i].style.backgroundColor=backgroundColor;
   }
   
-  
+  for (let i = 0; links.length; i++) {
+
+    links[i].style.color = backgroundColor;
+  }
   
 })
+
