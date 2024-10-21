ac763565a48e7e94968c5495d1b378c612eb5863
ViriatoF <viriato.ferreira44@gmail.com>
2024-10-21 13:41:08 +0200

    finish JS step 3 in part 1

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,7 +4,10 @@ let nameUser = document.querySelector("#firstname");
 
 const buttonModifyer = document.querySelector(".button_modifyer");
 
+const presentation = document.querySelector("#article_presentation");
+
 let newName = ""
+let newColor = ""
 
 function changeAvatar() {
   imgAvatar.src = "/site/image/avatar-bis.png"
@@ -18,6 +21,9 @@ function changeNameColor() {
   newName = prompt("Enter your name");
   nameUser.style.color = "white";
   nameUser.innerHTML = newName;
+  newColor = prompt("Choose a color");
+  console.log(newColor)
+  presentation.style.backgroundColor = newColor;
   
 };
 
