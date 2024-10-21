4de587d7fe50dc0b825ec861e21a4a4a4f735316
DamienD44 <damas44@gmail.com>
2024-10-21 12:31:41 +0200

    exo final non fini

---
+++ site/js/portfolio.js
@@ -0,0 +1,32 @@
+function imgChange() {
+    onClick = document.getElementById("avatar-bis") = '<img id="avatar" class="image" src="./image/avatar.svg"/> onClick = "toggle"';
+}
+
+function imgChange() {
+    onClick = document.getElementById("avatar-bis").src = "image/avatar.svg";
+}
+/*let div = document.createElement("button")
+function nameChange() {
+    document.getElementById("firstname")
+    let div = document.createElement("button")
+   
+
+}*/
+
+function name() {
+    let nameChange = getElementById("firstName")
+    let btn = document.createElement('button');
+    let input = document.createElement("input");
+    (input, btn).documentCreateElement
+
+
+
+
+}
+
+/*function creationbouton() {
+    let btn = document.createElement('button');
+    let input = document.getElementById('firstname')
+    btn.setAttribute('name', input.value)
+    input.parentElement.insertBefore(btn, input)
+}*/
