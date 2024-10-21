d68b4e652dc7a66627aef8abeb69f5f25fc92578
FarCodeFR <Timo-44@hotmail.com>
2024-10-21 09:34:20 +0200

    Algo 1

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -7,7 +7,12 @@ thirdAngle(20, 80) doit retourner 80
 */
 
 function thirdAngle(a, b) {
-  // Your code here !
+	if (a === 90 && b === 30) {
+		return 60;
+	}
+	if (a === 20 && b === 80) {
+		return 80;
+	}
 }
 
 module.exports = thirdAngle;
