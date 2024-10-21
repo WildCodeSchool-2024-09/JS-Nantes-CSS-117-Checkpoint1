092df93492e81dafe84047cfb53bc807cc621bb1
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2024-10-21 12:46:47 +0200

    portfolio done

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -12,14 +12,13 @@ initialAvatar.addEventListener("click", () => {
   switchAvatar(initialAvatar);
 });
 
-//CHANGE NAME, COLOR BACKGROUND AND ALL OTHERS ELEMENTS IN PINK
+//CHANGE NAME, COLOR BACKGROUND, TEXTS IN PINK , AND LINKS COLOR
 const firstName = document.querySelector("#firstname");
 const changeNameButton = document.querySelector(".changeNameButton");
 const pinkBackgrounds = document.querySelectorAll(".pink-bg");
 const pinkTexts = document.querySelectorAll(".pink-text");
-// const pinkElements = Array.from(pinkBackgrounds).concat(Array.from(pinkTexts));
-// console.log({ pinkElements });
-
+const bannerLinks = document.querySelectorAll(".menu a");
+console.log({ bannerLinks });
 const changeNameAndColor = () => {
   let userColor = prompt("Enter a color : ");
   pinkBackgrounds.forEach((element) => {
@@ -28,9 +27,22 @@ const changeNameAndColor = () => {
   pinkTexts.forEach((element) => {
     element.style.color = userColor;
   });
+  bannerLinks.forEach((link) => {
+    link.style.color = userColor;
+  });
   let userName = prompt("Enter your name : ");
   firstName.textContent = userName;
   firstName.style.color = "var(--light)";
 };
 
 changeNameButton.addEventListener("click", changeNameAndColor);
+
+//MODIFY SKILLS
+const skills = document.querySelectorAll("li[data-skill]");
+const skillsModifyButton = document.querySelector(".skillsModifyButton");
+
+skillsModifyButton.addEventListener("click", () => {
+  skills.forEach((skill) => {
+    skill.textContent = skill.dataset.skill;
+  });
+});
